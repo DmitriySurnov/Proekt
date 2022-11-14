f0573f55a9ad0dcb04dfb508ab572658318794da
DmitriySurnov <79646813832@yandex.ru>
2022-11-15 00:26:42 +0300

    часть игры готова

---
+++ Proekt/MainWindow.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int mapSize = 8;
+        const int cellSize = 50;
+        figure[,] simpleSteps = new figure[mapSize, mapSize];
+        int hod = 1;
+        int countWhiteFigure = 12;
+        int countBlackFigure = 12;
+        bool povtor = false;
+        figure b = null;
+        List<figure> Meal = null;
+        List<figure> lf = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "Шашки ходит игрок 1";
+            Init();
+        }
+        public void Init()
+        {
+            CreateMap();
+        }
+        public void CreateMap()
+        {
+            int[,] map = new int[mapSize, mapSize]{
+                { 0,1,0,1,0,1,0,1 },
+                { 1,0,1,0,1,0,1,0 },
+                { 0,1,0,1,0,1,0,1 },
+                { 0,0,0,0,0,0,0,0 },
+                { 0,0,0,0,0,0,0,0 },
+                { 2,0,2,0,2,0,2,0 },
+                { 0,2,0,2,0,2,0,2 },
+                { 2,0,2,0,2,0,2,0 }
+            };
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    simpleSteps[i, j] = new figure(i, j, map[j, i]);
+                    grids.Children.Add(simpleSteps[i, j].button);
+                    Grid.SetColumn(simpleSteps[i, j].button, i);
+                    Grid.SetRow(simpleSteps[i, j].button, j);
+                    simpleSteps[i, j].button.Click += Button_Click;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (b is null)
+                foreach (var i in simpleSteps)
+                {
+                    if ((Button)sender == i.button && i.Index == hod)
+                    {
+                        i.button.Background = Brushes.Red;
+                        b = i;
+                        Food_5(i.Row, i.Column);
+                        break;
+                    }
+                }
+            else if (b.button == (Button)sender)
+            {
+                otmena();
+                if (povtor)
+                    Perevod_hoda();
+            }
+            else if (((Button)sender).Background == Brushes.Green)
+            {
+                b.Index = 0;
+                b.Image();
+                foreach (var i in lf)
+                    if ((Button)sender == i.button)
+                    {
+                        i.Background();
+                        i.Index = hod;
+                        i.Image();
+                        if (!(Meal is null))
+                        {
+                            foreach (var m in Meal)
+                                if ((i.Row + 1 == m.Row || i.Row - 1 == m.Row) &&
+                                    (i.Column + 1 == m.Column || i.Column - 1 == m.Column))
+                                {
+                                    if (m.Index == 1)
+                                        countWhiteFigure--;
+                                    else
+                                        countBlackFigure--;
+                                    m.Index = 0;
+                                    m.Image();
+                                    otmena();
+                                    Food_5(i.Row, i.Column);
+                                    if (!(Meal is null))
+                                    {
+                                        povtor = true;
+                                        b = i;
+                                        b.button.Background = Brushes.Red;
+                                        foreach (var s in lf)
+                                            if ((i.Row + 1 == s.Row || i.Row - 1 == s.Row) &&
+                                            (i.Column + 1 == s.Column || i.Column - 1 == s.Column))
+                                                s.Background();
+                                        return;
+                                    }
+                                }
+                        }
+                        break;
+                    }
+                otmena();
+                Perevod_hoda();
+            }
+        }
+
+        private void Perevod_hoda()
+        {
+            if (hod == 1)
+            {
+                hod = 2;
+                Title = "Шашки ходит игрок 2";
+            }
+            else
+            {
+                hod = 1;
+                Title = "Шашки ходит игрок 1";
+            }
+            povtor = false;
+            if (countBlackFigure == 0)
+                Title = "поздравляем победитель игрок 1";
+            else if (countWhiteFigure == 0)
+                Title = "поздравляем победитель игрок 2";
+            else
+            {
+                if (hod == 1 && countWhiteFigure == 1)
+                {
+                    foreach (var i in simpleSteps)
+                    {
+                        if (i.Index == hod)
+                        {
+                            Food_5(i.Row, i.Column);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void otmena()
+        {
+            if (!(b is null))
+                b.Background();
+            b = null;
+            if (!(lf is null))
+                foreach (var i in lf)
+                    i.Background();
+            lf = null;
+            Meal = null;
+        }
+        private void PossibleMove(int i, int j)
+        {
+            if (simpleSteps[i, j].Index == 0)
+            {
+                simpleSteps[i, j].button.Background = Brushes.Green;
+                if (lf is null)
+                    lf = new List<figure>();
+                lf.Add(simpleSteps[i, j]);
+            }
+        }
+        private void Food_1(int i, int j)
+        {
+            if (simpleSteps[i, j].Index == ((hod == 1) ? 2 : 1))
+                if (i + 1 < mapSize && j - 1 >= 0)
+                {
+                    PossibleMove(i + 1, j - 1);
+                    if (simpleSteps[i + 1, j - 1].button.Background == Brushes.Green)
+                    {
+                        if (Meal is null)
+                            Meal = new List<figure>();
+                        Meal.Add(simpleSteps[i, j]);
+                    }
+                }
+        }
+        private void Food_2(int i, int j)
+        {
+            if (simpleSteps[i, j].Index == ((hod == 1) ? 2 : 1))
+                if (i - 1 >= 0 && j - 1 >= 0)
+                {
+                    PossibleMove(i - 1, j - 1);
+                    if (simpleSteps[i - 1, j - 1].button.Background == Brushes.Green)
+                    {
+                        if (Meal is null)
+                            Meal = new List<figure>();
+                        Meal.Add(simpleSteps[i, j]);
+                    }
+                }
+        }
+        private void Food_3(int i, int j)
+        {
+            if (simpleSteps[i, j].Index == ((hod == 1) ? 2 : 1))
+                if (i - 1 >= 0 && j + 1 < mapSize)
+                {
+                    PossibleMove(i - 1, j + 1);
+                    if (simpleSteps[i - 1, j + 1].button.Background == Brushes.Green)
+                    {
+                        if (Meal is null)
+                            Meal = new List<figure>();
+                        Meal.Add(simpleSteps[i, j]);
+                    }
+                }
+        }
+        private void Food_4(int i, int j)
+        {
+            if (simpleSteps[i, j].Index == ((hod == 1) ? 2 : 1))
+
+                if (i + 1 < mapSize && j + 1 < mapSize)
+                {
+                    PossibleMove(i + 1, j + 1);
+                    if (simpleSteps[i + 1, j + 1].button.Background == Brushes.Green)
+                    {
+                        if (Meal is null)
+                            Meal = new List<figure>();
+                        Meal.Add(simpleSteps[i, j]);
+                    }
+                }
+        }
+        private void Food_5(int i, int j)
+        {
+            if (i + 1 < mapSize && j - 1 >= 0)
+            {
+                if (hod == 2)
+                    PossibleMove(i + 1, j - 1);
+                Food_1(i + 1, j - 1);
+            }
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                if (hod == 2)
+                    PossibleMove(i - 1, j - 1);
+                Food_2(i - 1, j - 1);
+            }
+            if (i - 1 >= 0 && j + 1 < mapSize)
+            {
+                if (hod == 1)
+                    PossibleMove(i - 1, j + 1);
+                Food_3(i - 1, j + 1);
+            }
+            if (i + 1 < mapSize && j + 1 < mapSize)
+            {
+                if (hod == 1)
+                    PossibleMove(i + 1, j + 1);
+                Food_4(i + 1, j + 1);
+            }
+        }
+    }
+}
---
+++ Proekt/figure.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp3
+{
+    public class ImageFigure
+    {
+        public Image WhiteFigure(int cellSize)
+        {
+            return Figure(cellSize, @"D:\С# form\22.11.07\Zadanie\WpfApp3\Sprites\w.png");
+        }
+
+        private Image Figure(int cellSize,string uri)
+        {
+            Image ImageFigure = new Image();
+            ImageFigure.Width = cellSize - 10;
+            ImageFigure.Height = cellSize - 10;
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(uri);
+            myBitmapImage.DecodePixelWidth = cellSize - 10;
+            myBitmapImage.DecodePixelHeight = cellSize - 10;
+            myBitmapImage.EndInit();
+            ImageFigure.Source = myBitmapImage;
+            return ImageFigure;
+        }
+
+        public Image BlackFigure(int cellSize)
+        {
+            return Figure(cellSize, @"D:\С# form\22.11.07\Zadanie\WpfApp3\Sprites\b.png");
+        }
+    }
+
+
+    class figure
+    {
+        private const int cellSize = 50;
+        private ImageFigure Figure = new ImageFigure();
+        public Button button;
+        public int Index;
+        public int Row;
+        public int Column;
+
+        public figure(int row, int column, int index)
+        {
+            Index = index;
+            Row = row;
+            Column = column;
+            button = new Button();
+            Background();
+            Image();
+        }
+
+        public void Image()
+        {
+            if (Index == 1)
+                button.Content = Figure.WhiteFigure(cellSize);
+            else if (Index == 2)
+                button.Content = Figure.BlackFigure(cellSize);
+            else
+                button.Content = "";
+        }
+
+        public void Background()
+        {
+            if (Row % 2 == 0)
+            {
+                if (Column % 2 == 0)
+                    button.Background = Brushes.White;
+                else
+                    button.Background = Brushes.Black;
+            }
+            else
+            {
+                if (Column % 2 == 1)
+                    button.Background = Brushes.White;
+                else
+                    button.Background = Brushes.Black;
+            }
+        }
+    }
+}
